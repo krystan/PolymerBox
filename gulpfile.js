a1dac824a9a61d27c0eaf878542e6b617b9e45d2
Krystan Honour <krystan@gmail.com>
2015-10-06 10:41:41 +0000

    changed ports to be compatible with c9.io

--- gulpfile.js
+++ gulpfile.js
@@ -229,7 +229,7 @@ gulp.task('clean', function (cb) {
 // Watch files for changes & reload
 gulp.task('serve', ['styles', 'elements', 'images'], function () {
   browserSync({
-    port: 5000,
+    port: 8080,
     notify: false,
     logPrefix: 'PSK',
     snippetOptions: {
@@ -263,7 +263,7 @@ gulp.task('serve', ['styles', 'elements', 'images'], function () {
 // Build and serve the output from the dist build
 gulp.task('serve:dist', ['default'], function () {
   browserSync({
-    port: 5001,
+    port: 8081,
     notify: false,
     logPrefix: 'PSK',
     snippetOptions: {
